8fd67bfe8d28a20e6ed6162d78c453c449aac48b
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2024-08-04 23:07:19 +0200

    chore: setup ts repo

---
+++ src/data.ts
@@ -0,0 +1,402 @@
+export const data = [
+  {
+    years: 1,
+    gift: "Coton",
+  },
+  {
+    years: 2,
+    gift: "Cuir",
+  },
+  {
+    years: 3,
+    gift: "Froment",
+  },
+  {
+    years: 4,
+    gift: "Cire",
+  },
+  {
+    years: 5,
+    gift: "Bois",
+  },
+  {
+    years: 6,
+    gift: "Chypre",
+  },
+  {
+    years: 7,
+    gift: "Laine",
+  },
+  {
+    years: 8,
+    gift: "Coquelicot",
+  },
+  {
+    years: 9,
+    gift: "Faïence",
+  },
+  {
+    years: 10,
+    gift: "Étain",
+  },
+  {
+    years: 11,
+    gift: "Corail",
+  },
+  {
+    years: 12,
+    gift: "Soie",
+  },
+  {
+    years: 13,
+    gift: "Muguet",
+  },
+  {
+    years: 14,
+    gift: "Plomb",
+  },
+  {
+    years: 15,
+    gift: "Cristal",
+  },
+  {
+    years: 16,
+    gift: "Saphir",
+  },
+  {
+    years: 17,
+    gift: "Rose",
+  },
+  {
+    years: 18,
+    gift: "Turquoise",
+  },
+  {
+    years: 19,
+    gift: "Cretonne",
+  },
+  {
+    years: 20,
+    gift: "Porcelaine",
+  },
+  {
+    years: 21,
+    gift: "Opale",
+  },
+  {
+    years: 22,
+    gift: "Bronze",
+  },
+  {
+    years: 23,
+    gift: "Béryl",
+  },
+  {
+    years: 24,
+    gift: "Satin",
+  },
+  {
+    years: 25,
+    gift: "Argent",
+  },
+  {
+    years: 26,
+    gift: "Jade",
+  },
+  {
+    years: 27,
+    gift: "Acajou",
+  },
+  {
+    years: 28,
+    gift: "Nickel",
+  },
+  {
+    years: 29,
+    gift: "Velours",
+  },
+  {
+    years: 30,
+    gift: "Perle",
+  },
+  {
+    years: 31,
+    gift: "Basane",
+  },
+  {
+    years: 32,
+    gift: "Cuivre",
+  },
+  {
+    years: 33,
+    gift: "Porphyre",
+  },
+  {
+    years: 34,
+    gift: "Ambre",
+  },
+  {
+    years: 35,
+    gift: "Rubis",
+  },
+  {
+    years: 36,
+    gift: "Mousseline",
+  },
+  {
+    years: 37,
+    gift: "Papier",
+  },
+  {
+    years: 38,
+    gift: "Mercure",
+  },
+  {
+    years: 39,
+    gift: "Crêpe",
+  },
+  {
+    years: 40,
+    gift: "Émeraude",
+  },
+  {
+    years: 41,
+    gift: "Fer",
+  },
+  {
+    years: 42,
+    gift: "Nacre",
+  },
+  {
+    years: 43,
+    gift: "Flanelle",
+  },
+  {
+    years: 44,
+    gift: "Topaze",
+  },
+  {
+    years: 45,
+    gift: "Vermeil",
+  },
+  {
+    years: 46,
+    gift: "Lavande",
+  },
+  {
+    years: 47,
+    gift: "Cachemire",
+  },
+  {
+    years: 48,
+    gift: "Améthyste",
+  },
+  {
+    years: 49,
+    gift: "Cèdre",
+  },
+  {
+    years: 50,
+    gift: "Or",
+  },
+  {
+    years: 51,
+    gift: "Camélia",
+  },
+  {
+    years: 52,
+    gift: "Tourmaline",
+  },
+  {
+    years: 53,
+    gift: "Merisier",
+  },
+  {
+    years: 54,
+    gift: "Zibeline",
+  },
+  {
+    years: 55,
+    gift: "Orchidée",
+  },
+  {
+    years: 56,
+    gift: "Buis/lapis-lazuli\n(selon les références)",
+  },
+  {
+    years: 57,
+    gift: "Azalée",
+  },
+  {
+    years: 58,
+    gift: "Érable",
+  },
+  {
+    years: 59,
+    gift: "Olivier/vison\n(selon les références)",
+  },
+  {
+    years: 60,
+    gift: "Diamant",
+  },
+  {
+    years: 61,
+    gift: "Platane",
+  },
+  {
+    years: 62,
+    gift: "Ivoire",
+  },
+  {
+    years: 63,
+    gift: "Lilas",
+  },
+  {
+    years: 64,
+    gift: "Astrakan",
+  },
+  {
+    years: 65,
+    gift: "Palissandre",
+  },
+  {
+    years: 66,
+    gift: "Jasmin",
+  },
+  {
+    years: 67,
+    gift: "Chinchilla",
+  },
+  {
+    years: 68,
+    gift: "Granit",
+  },
+  {
+    years: 69,
+    gift: "Mélèze",
+  },
+  {
+    years: 70,
+    gift: "Platine",
+  },
+  {
+    years: 71,
+    gift: "Ocre",
+  },
+  {
+    years: 72,
+    gift: "Titane",
+  },
+  {
+    years: 73,
+    gift: "Fonte",
+  },
+  {
+    years: 74,
+    gift: "Sienne",
+  },
+  {
+    years: 75,
+    gift: "Albâtre",
+  },
+  {
+    years: 76,
+    gift: "Zinc",
+  },
+  {
+    years: 77,
+    gift: "Silice",
+  },
+  {
+    years: 78,
+    gift: "Ébène",
+  },
+  {
+    years: 79,
+    gift: "Bambou",
+  },
+  {
+    years: 80,
+    gift: "Chêne",
+  },
+  {
+    years: 81,
+    gift: "Cornaline",
+  },
+  {
+    years: 82,
+    gift: "Grenat",
+  },
+  {
+    years: 83,
+    gift: "Vanille",
+  },
+  {
+    years: 84,
+    gift: "Iris",
+  },
+  {
+    years: 85,
+    gift: "Uranium",
+  },
+  {
+    years: 86,
+    gift: "Jaspe",
+  },
+  {
+    years: 87,
+    gift: "Psilocybe",
+  },
+  {
+    years: 88,
+    gift: "Pehuen",
+  },
+  {
+    years: 89,
+    gift: "Onyx",
+  },
+  {
+    years: 90,
+    gift: "Granite",
+  },
+  {
+    years: 91,
+    gift: "Héliodore",
+  },
+  {
+    years: 92,
+    gift: "Lys",
+  },
+  {
+    years: 93,
+    gift: "Lapis-lazuli",
+  },
+  {
+    years: 94,
+    gift: "Péridot",
+  },
+  {
+    years: 95,
+    gift: "Adansonia",
+  },
+  {
+    years: 96,
+    gift: "Potasse",
+  },
+  {
+    years: 97,
+    gift: "Tourmaline noire",
+  },
+  {
+    years: 98,
+    gift: "If",
+  },
+  {
+    years: 99,
+    gift: "Araucaria",
+  },
+  {
+    years: 100,
+    gift: "Eau",
+  },
+];
